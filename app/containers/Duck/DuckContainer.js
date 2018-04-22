90cc2421a420a0fff34e6f6e8b80f2e0485bd215
Sean Hallahan <sfhallahan@gmail.com>
2018-04-22 18:45:27 -0400

    temp removing user profile

--- app/containers/Duck/DuckContainer.js
+++ app/containers/Duck/DuckContainer.js
@@ -1,38 +1,39 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import { bindActionCreators } from 'redux'
-import { connect } from 'react-redux'
-import { Duck } from 'components'
-import * as usersLikesActionCreators from 'redux/modules/usersLikes'
-
+import React from "react";
+import PropTypes from "prop-types";
+import { bindActionCreators } from "redux";
+import { connect } from "react-redux";
+import { Duck } from "components";
+import * as usersLikesActionCreators from "redux/modules/usersLikes";
 
 class DuckContainer extends React.Component {
   constructor(props) {
-    super(props)
-    this.handleClick = this.handleClick.bind(this)
-    this.goToProfile = this.goToProfile.bind(this)
+    super(props);
+    this.handleClick = this.handleClick.bind(this);
+    this.goToProfile = this.goToProfile.bind(this);
   }
 
-  goToProfile (e) {
-    e.stopPropagation()
-    this.context.router.push('/' + this.props.duck.get('uid'))
+  goToProfile(e) {
+    e.stopPropagation();
+    // needs to be updated to fetch other users profiles
+    // this.context.router.push('/' + this.props.duck.get('uid'))
   }
-  handleClick (e) {
-     e.stopPropagation()
-     this.context.router.push('/duckDetail/' + this.props.duck.get('duckId'))
+  handleClick(e) {
+    e.stopPropagation();
+    this.context.router.push("/duckDetail/" + this.props.duck.get("duckId"));
   }
 
-  render () {
+  render() {
     return (
       <Duck
-        goToProfile={this.goToProfile}
+        goToProfile={this.props.hideReplyBtn === true ? null : this.handleClick}
         onClick={this.props.hideReplyBtn === true ? null : this.handleClick}
-        {...this.props} />
-    )
+        {...this.props}
+      />
+    );
   }
 }
 
-const { func, object, bool, number } = PropTypes
+const { func, object, bool, number } = PropTypes;
 DuckContainer.propTypes = {
   duck: object.isRequired,
   numberOfLikes: number,
@@ -40,33 +41,30 @@ DuckContainer.propTypes = {
   hideLikeCount: bool.isRequired,
   hideReplyBtn: bool.isRequired,
   handleDeleteLike: func.isRequired,
-  addAndHandleLike: func.isRequired,
-}
+  addAndHandleLike: func.isRequired
+};
 
 DuckContainer.defaultProps = {
   hideReplyBtn: false,
-  hideLikeCount: true,
-}
+  hideLikeCount: true
+};
 
 DuckContainer.contextTypes = {
-  router: PropTypes.object.isRequired,
-}
+  router: PropTypes.object.isRequired
+};
 
-function mapStateToProps({ducks, likeCount, usersLikes}, props) {
+function mapStateToProps({ ducks, likeCount, usersLikes }, props) {
   return {
     duck: ducks.get(props.duckId),
     hideLikeCount: props.hideLikeCount,
     hideReplyBtn: props.hideReplyBtn,
     isLiked: usersLikes[props.duckId] === true,
-    numberOfLikes: likeCount.get(props.duckId),
-  }
+    numberOfLikes: likeCount.get(props.duckId)
+  };
 }
 
 function mapDispatchToProps(dispatch) {
-  return bindActionCreators(usersLikesActionCreators, dispatch)
+  return bindActionCreators(usersLikesActionCreators, dispatch);
 }
 
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps,
-)(DuckContainer)
+export default connect(mapStateToProps, mapDispatchToProps)(DuckContainer);
